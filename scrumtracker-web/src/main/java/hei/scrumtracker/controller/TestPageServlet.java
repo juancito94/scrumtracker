97ed34604ac711fb94c342b5cf6db231fbc1fa98
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-07 11:00:10 +0100

    pom + servletTest begin

---
+++ scrumtracker-web/src/main/java/hei/scrumtracker/controller/TestPageServlet.java
@@ -0,0 +1,9 @@
+package hei.scrumtracker.controller;
+
+/**
+ * Created by thiba on 07/02/2017.
+ */
+//@WebServlet(name = "TestPageServlet", urlPatterns = "/*")
+public class TestPageServlet {
+//extends javax.servlet.http.HttpServlet
+}
