d12f2135574fe64a0733aa4dacae7794b944f7b3
ThibaultG1 <tiboyoyo@gmail.com>
2017-02-07 13:23:34 +0100

    deleteTestPAgeServlet

--- scrumtracker-web/src/main/java/hei/scrumtracker/controller/TestPageServlet.java
+++
@@ -1,9 +0,0 @@
-package hei.scrumtracker.controller;
-
-/**
- * Created by thiba on 07/02/2017.
- */
-//@WebServlet(name = "TestPageServlet", urlPatterns = "/*")
-public class TestPageServlet {
-//extends javax.servlet.http.HttpServlet
-}
